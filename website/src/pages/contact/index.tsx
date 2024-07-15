0ad0ad80f1d19384906a6c0fb95589eb53b97597
Hubert Zhang <zhanghuan929@gmail.com>
2024-07-15 11:55:34 +0800

    Add bg png for contact

--- website/src/pages/contact/index.tsx
+++ website/src/pages/contact/index.tsx
@@ -18,87 +18,89 @@ const ContactPage: React.FC = () => {
 
   return (
     <Layout title="Contact Us">
-      <div className="contact-page">
-        <div className="top-description">
-          <h1>GETTING IN TOUCH</h1>
-          <p>
-            Please reach out to us if you have any questions regarding sales,
-            support, or general inquiries!
-          </p>
-        </div>
-        <div className="content">
-          <div className="info-panel">
-            <div className="info-section">
-              <h2>Chat with Us</h2>
-              <p>
-                Feel free to chat with us on{" "}
-                <a
-                  href="https://www.eloqdata.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="red-link"
-                >
-                  Discord
-                </a>{" "}
-                for any inquiries, sales, product demos, information, or
-                support.
-              </p>
-            </div>
-            <div className="info-section">
-              <h2>Contact Form</h2>
-              <p>
-                For all other inquiries, please fill out this contact form, and
-                we will get back to you within one business day.
-              </p>
-            </div>
+      <div className="contact-page-container">
+        <div className="contact-page">
+          <div className="top-description">
+            <h1>GETTING IN TOUCH</h1>
+            <p>
+              Please reach out to us if you have any questions regarding sales,
+              support, or general inquiries!
+            </p>
           </div>
-          <div className="form-panel">
-            <form
-              action="https://getform.io/f/bmdpkgja"
-              method="POST"
-              className="contact-form"
-            >
-              <label className="required">
-                Name:
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                  name="name"
-                />
-              </label>
-              <label className="required">
-                Email:
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  name="email"
-                />
-              </label>
-              <label>
-                Company:
-                <input
-                  type="text"
-                  value={company}
-                  onChange={(e) => setCompany(e.target.value)}
-                  name="company"
-                />
-              </label>
-              <label className="required">
-                Message:
-                <textarea
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  rows={5}
-                  required
-                  name="message"
-                ></textarea>
-              </label>
-              <button type="submit">Submit</button>
-            </form>
+          <div className="content">
+            <div className="info-panel">
+              <div className="info-section">
+                <h2>Chat with Us</h2>
+                <p>
+                  Feel free to chat with us on{" "}
+                  <a
+                    href="https://www.eloqdata.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="red-link"
+                  >
+                    Discord
+                  </a>{" "}
+                  for any inquiries, sales, product demos, information, or
+                  support.
+                </p>
+              </div>
+              <div className="info-section">
+                <h2>Contact Form</h2>
+                <p>
+                  For all other inquiries, please fill out this contact form,
+                  and we will get back to you within one business day.
+                </p>
+              </div>
+            </div>
+            <div className="form-panel">
+              <form
+                action="https://getform.io/f/bmdpkgja"
+                method="POST"
+                className="contact-form"
+              >
+                <label className="required">
+                  Name:
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    name="name"
+                  />
+                </label>
+                <label className="required">
+                  Email:
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    name="email"
+                  />
+                </label>
+                <label>
+                  Company:
+                  <input
+                    type="text"
+                    value={company}
+                    onChange={(e) => setCompany(e.target.value)}
+                    name="company"
+                  />
+                </label>
+                <label className="required">
+                  Message:
+                  <textarea
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    rows={5}
+                    required
+                    name="message"
+                  ></textarea>
+                </label>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
