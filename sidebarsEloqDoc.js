5112ee77b1404e2351c14a5eb013bd27c26876b3
Hubert Zhang <zhanghuan929@gmail.com>
2025-09-29 16:23:43 -0700

    Update mobile css

--- sidebarsEloqDoc.js
+++ sidebarsEloqDoc.js
@@ -15,7 +15,7 @@
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   eloqdoc: {
-$   'Get Started': ['install-from-binary', 'install-from-binary-object-storage'],
+   'Get Started': ['install-from-binary', 'install-from-binary-object-storage'],
   },
 };
 module.exports = sidebars;
